c0f71e236f66be0b22c86162e82b9977d0c5373e
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-04-24 20:18:57 -0400

    add, change, toggle, and clear
    app js declarations

--- src/App.js
+++ src/App.js
@@ -6,6 +6,55 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'React Homework',
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'Learn Redux',
+          id: 2,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
+  addTodo = e => {
+    e.preventDefault();
+    const newTodo = { task: this.state.todo, completed: false, id: Date.now()};
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+      todo: ''
+    });
+  };
+
+  changeTodo = e => this.setState({ [e.target.name]: e.target.value });
+
+  toggleTodoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = e => {
+    e.preventDefault();
+    let todos = this.state.todos.filter(todo => !todo.completed);
+    this.setState({ todos });
+  };
   render() {
     return (
       <div>
